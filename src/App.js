a18a4f9320c3b5c853d01d4e9d24325fad30b6b1
yerm <leeyelim98@gmail.com>
2024-07-11 17:17:51 +0900

    20240711 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+// import '../public/css/styles.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import PortFolio from './components/PortFolio';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Copyright from './components/Copyright';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Header />
+      <PortFolio />
+      <About />
+      <Contact />
+      <Footer />
+      <Copyright />
     </div>
   );
 }
---
+++ src/components/Copyright.jsx
@@ -0,0 +1,9 @@
+const CopyRight = () => {
+    return (
+        <div className="copyright py-4 text-center text-white">
+            <div className="container"><small>Copyright &copy; Your Website 2023</small></div>
+        </div>
+    );
+};
+
+export default CopyRight;
